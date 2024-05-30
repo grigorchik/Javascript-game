3b9e578d13bf5b0e8d6113ce6458a141ab689e1a
Grigorchik Alexandr <grigorchik.alexandr@gmail.com>
2024-05-30 16:43:23 +0300

    2

---
+++ src/Player.js
@@ -0,0 +1,27 @@
+class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 120;
+        this.height = 190;
+        this.x = 20;
+        this.y = 100;
+        this.speedY = 0;
+        this.maxSpeed = 8;
+    }
+
+    update() {
+        this.y += this.speedY;
+
+        if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed
+        else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed
+        else this.speedY = 0;
+
+        if (this.y > this.game.height - this.height * 0.5) this.y = this.game.
+        height - this.height * 0.5;
+        else if (this.y < -this.height * 0.5) this.y = -this.height * 0.5;
+    }
+
+    draw(context) {
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
